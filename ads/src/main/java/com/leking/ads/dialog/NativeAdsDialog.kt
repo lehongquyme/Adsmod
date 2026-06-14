625740cf06d622cf871840b8ff90aaea43d03da5
lehongquyme <lehongquy.me@gmail.com>
2026-06-14 18:31:32 +0700

    Update Ads

--- ads/src/main/java/com/leking/ads/dialog/NativeAdsDialog.kt
+++ ads/src/main/java/com/leking/ads/dialog/NativeAdsDialog.kt
@@ -17,6 +17,7 @@ class NativeAdsDialog(context: Context) : Dialog(context, R.style.AdsDialogTheme
 
         val container = findViewById<FrameLayout>(R.id.native_ad_container)
         container.removeAllViews()
+
         val nativeView = LayoutInflater.from(context)
             .inflate(R.layout.ads_native_dialog, container, false)
 
